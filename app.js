f42ed142d0e7a332bbaaf04c74fdfd084fc26f30
reicoastdev <117753363+reicoastdev@users.noreply.github.com>
2023-12-05 20:07:37 -0300

    Aula - Primeira rota com Express

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const express = require('express')
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Minha Lista de Tarefas</h1>");
+  res.statusCode = 200;
+});
+
+app.listen(3000, () => {
+  console.log("Servidor ativo!");
+});
